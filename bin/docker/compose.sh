40b3aeb42b6538edef3ece54419c0cb2a4b57caf
James Ding <james.ding.illinois@gmail.com>
2019-12-07 22:35:35 -0600

    compose.sh beef up

--- bin/docker/compose.sh
+++ bin/docker/compose.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+set -x
+
+# Build project
 gradle clean build
+
+# Build docker images
 sudo docker-compose build
+
+# Kill local postgres database to free up port 5432
+if [ `which systemctl` -a `which psql` ]; then
+    systemctl stop postgresql
+fi
+
+# Start service container
 sudo docker-compose up
